d0d9a7baa4775579c051d38c93ca059936ad9f0e
motojouya <motojouya@gmail.com>
2019-04-15 21:39:32 +0900

    maybe last

--- init.sh
+++ init.sh
@@ -7,10 +7,11 @@ export AWS_DEFAULT_REGION=ap-northeast-1
 region=$1
 userid=$2
 username=$3
-ssh_port=$4
-hosted_zone_id=$5
-domain=$6
-volume_id=$7
+password=$4
+ssh_port=$5
+hosted_zone_id=$6
+domain=$7
+volume_id=$8
 
 instance_id=$(curl -s 169.254.169.254/latest/meta-data/instance-id)
 ip=$(curl -s 169.254.169.254/latest/meta-data/public-ipv4)
@@ -33,7 +34,8 @@ useradd -u $userid -d /home/$username -s /bin/bash $username
 gpasswd -a $username sudo
 cp -arpf /home/ubuntu/.ssh /home/$username/
 chown -R $username /home/$username/.ssh
-# userdel -r ubuntu
+echo "$username:$password" | chpasswd
+userdel -r ubuntu
 
 # register route53
 curl https://raw.githubusercontent.com/motojouya/ec2-develop/master/dyndns.tmpl -O
