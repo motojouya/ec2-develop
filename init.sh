25921edbaa8d0d264bc2715da5ae07bcdbdca207
motojouya <motojouya@gmail.com>
2019-04-18 12:51:05 +0000

    install tools

--- init.sh
+++ init.sh
@@ -18,10 +18,9 @@ ip=$(curl -s 169.254.169.254/latest/meta-data/public-ipv4)
 
 cd /home/ubuntu
 
-# update install
+# install awscli
 apt update
 apt install -y python3-pip
-apt install -y neovim
 pip3 install awscli
 
 # mount ebs volume
@@ -37,7 +36,7 @@ mount /dev/nvme1n1 /home/$username
 # add user
 useradd -u $userid -d /home/$username -s /bin/bash $username
 gpasswd -a $username sudo
-cp -arpf /home/ubuntu/.ssh /home/$username/
+cp -arpf /home/ubuntu/.ssh/authorized_keys /home/$username/.ssh/authorized_keys
 chown $username /home/$username
 chgrp $username /home/$username
 chown -R $username /home/$username/.ssh
@@ -54,6 +53,37 @@ curl https://raw.githubusercontent.com/motojouya/ec2-develop/master/sshd_config.
 sed -e s/{%port%}/$ssh_port/g sshd_config.tmpl > sshd_config.init
 cp sshd_config.init /etc/ssh/sshd_config
 systemctl restart sshd
+
+# install nodejs
+apt install -y nodejs
+apt install -y npm
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+apt update
+apt install -y yarn
+
+# install docker
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# install vim
+apt install -y neovim
 
 cd /
 userdel -r ubuntu
