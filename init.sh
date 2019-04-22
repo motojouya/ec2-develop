8e3916c9af70b8f86f454d5cc998789438b11b9d
motojouya <motojouya@gmail.com>
2019-04-22 10:28:46 +0000

    docker sudo

--- init.sh
+++ init.sh
@@ -82,6 +82,9 @@ apt-get install -y docker-ce docker-ce-cli containerd.io
 curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+gpasswd -a $username docker
+systemctl restart docker
+
 # install vim
 apt install -y neovim
 
