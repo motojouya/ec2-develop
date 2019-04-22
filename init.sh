275234c439ebaba6e6e3c5a41e32f6e4d7773062
motojouya <motojouya@gmail.com>
2019-04-22 11:39:11 +0000

    docker-compose version

--- init.sh
+++ init.sh
@@ -79,7 +79,7 @@ add-apt-repository \
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
-curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 gpasswd -a $username docker
